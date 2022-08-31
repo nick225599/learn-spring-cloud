52ee32fba6a49521dc4344bb4e09a9cddff2e284
nick225599 <nick225599@163.com>
2022-08-31 21:29:51 +0800

    恢复了 eureka 分布式方案

--- cloud-consumer-order80-eureka/src/main/java/org/example/springcloud/payment/order/Order80Eureka.java
+++ cloud-consumer-order80-eureka/src/main/java/org/example/springcloud/payment/order/Order80Eureka.java
@@ -1,11 +1,17 @@
 package org.example.springcloud.payment.order;
 
+//import org.example.springcloud.payment.ribbonrule.RandomRuleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.ribbon.RibbonClient;
+//import org.springframework.cloud.netflix.ribbon.RibbonClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
+//@RibbonClient(name="CLOUD-PAYMENT-SERVICE", configuration = RandomRuleConfig.class)
 public class Order80Eureka {
     public static void main(String[] args) {
         SpringApplication.run(Order80Eureka.class, args);
---
+++ cloud-consumer-order80-eureka/src/main/java/org/example/springcloud/payment/ribbonrule/RandomRuleConfig.java
@@ -0,0 +1,15 @@
+//package org.example.springcloud.payment.ribbonrule;
+//
+//import com.netflix.loadbalancer.IRule;
+//import com.netflix.loadbalancer.RandomRule;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class RandomRuleConfig {
+//
+//    @Bean
+//    public IRule ribbonRule(){
+//        return new RandomRule();
+//    }
+//}
