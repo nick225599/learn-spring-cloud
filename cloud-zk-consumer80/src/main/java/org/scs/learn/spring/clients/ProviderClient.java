c12c8d60408b4f6ebe9c30ead88de720d29730bb
nick225599 <nick225599@163.com>
2022-09-05 21:49:22 +0800

    hystrix dashboard - 搭建成功

--- cloud-zk-consumer80/src/main/java/org/scs/learn/spring/clients/ProviderClient.java
+++ cloud-zk-consumer80/src/main/java/org/scs/learn/spring/clients/ProviderClient.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "zk-provider")
+@FeignClient(value = "zk-provider", fallback = Object.class)
 @Component
 public interface ProviderClient {
 
